c5338eadd1f324df1cc8970e7cdf4f5c5f808242
Sadman Sameer <itsnotsameer@gmail.com>
2024-03-20 00:25:51 -0400

    alternate soln added for 19

--- Easy/valid palindrome.py
+++ Easy/valid palindrome.py
@@ -18,3 +18,18 @@ class Solution(object):
         # return true if the string is the same as its reverse
         return newStr == newStr[::-1]
         
+    # Alternate solution (memory efficient)
+    def isPalindrome(self, s):
+        l, r = 0, len(s) - 1
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+    
