eec51b6c14da6cc3f069dc6b556ed84df020a5e2
Sadman Sameer <itsnotsameer@gmail.com>
2024-03-24 18:23:41 -0400

    alternate solns added for 26

--- Easy/counting bits.py
+++ Easy/counting bits.py
@@ -14,6 +14,18 @@ class Solution(object):
             ans.append(binRep.count('1'))
         return ans
 
-        
+        # O(n) solution
+        ans = [0] * (n+1)
+        for i in range(1, n+1):
+            ans[i] =  1 + ans[i & (i-1)]
+        return ans
+    
+        # Another O(n) solution
+        dp = [0] * (n+1)
+        offset = 1
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] =  1 + dp[i - offset]
 
         
