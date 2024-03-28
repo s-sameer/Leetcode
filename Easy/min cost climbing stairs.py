618caf7319b664384dd28e057a75afdac2899b6d
Sadman Sameer <itsnotsameer@gmail.com>
2024-03-27 21:04:13 -0400

    31st problem added

---
+++ Easy/min cost climbing stairs.py
@@ -0,0 +1,17 @@
+'''
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+'''
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # We want to start from the end, not the beginning, and go in reverse
+        # This time we'll start from the second last element
+        cost.append(0) # To make the math work out
+        # min cos to reach dest from current pos is min of the next two pos
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] = cost[i] + min(cost[i+1], cost[i+2])
+        
+        return min(cost[0], cost[1])
