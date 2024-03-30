d7206bbc59424abe220811f00510a5fe34f2a600
Sadman Sameer <itsnotsameer@gmail.com>
2024-03-30 16:41:52 -0400

    added explanations

--- Easy/reverse bits.py
+++ Easy/reverse bits.py
@@ -4,8 +4,10 @@ Reverse bits of a given 32 bits unsigned integer.
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
-        for _ in range(32):
-            res <<= 1
-            res = res | (n & 1)
-            n >>= 1
+        for _ in range(32): # iterate 32 times bc 32 bits
+            res <<= 1 # shift res to the left by 1 bit to make room for the next bit
+            res = res | (n & 1) # add the last bit of n to res
+            n >>= 1 # shift n to the right by 1 bit to get the next bit
         return res
+    
+# We need to shift before adding the next bit and not after adding it bc it will result in a extra 0 at the end of res
