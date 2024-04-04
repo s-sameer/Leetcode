4e6c1ec980222e8882f92e9febe334c4df740981
Sadman Sameer <itsnotsameer@gmail.com>
2024-04-03 20:16:15 -0400

    44th problem added

--- Medium/product of subarray except itself.py
+++ Medium/product of subarray except itself.py
@@ -5,3 +5,15 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
 
 You must write an algorithm that runs in O(n) time and without using the division operation.
 '''
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        prefix  = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
