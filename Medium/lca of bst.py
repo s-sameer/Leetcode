3cd6bdcaddea95cd1001e15f57fae07f42ed16da
Sadman Sameer <itsnotsameer@gmail.com>
2024-04-04 23:49:54 -0400

    47th problem added

---
+++ Medium/lca of bst.py
@@ -0,0 +1,17 @@
+'''
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+'''
+# Intuition: LCA is where the two nodes split. If the two nodes are on the same side of the root, then the LCA is on that side. If the two nodes are on different sides of the root, then the LCA is the root.
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                # The two nodes are on different sides of the root
+                return curr
