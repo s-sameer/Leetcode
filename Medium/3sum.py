4e0adc1d5c9966cba40096b33c784489a71e0d1f
Sadman Sameer <itsnotsameer@gmail.com>
2024-04-08 20:46:24 -0400

    new question added

---
+++ Medium/3sum.py
@@ -0,0 +1,6 @@
+'''
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+'''
+# Intuition: sort the array, pick one element and then use two pointer approach to find the other two elements.
