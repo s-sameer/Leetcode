1c6bd1318064431ab1988cb8869e156908e575d3
Sadman Sameer <itsnotsameer@gmail.com>
2024-04-13 02:04:50 -0400

    57th problem added

--- Medium/container with most water.py
+++ Medium/container with most water.py
@@ -6,4 +6,17 @@ Find two lines that together with the x-axis form a container, such that the con
 Return the maximum amount of water a container can store.
 '''
 # Intuition: The area formed between the lines will always be limited by the height of the shorter line.
-# Brute force approach 
+# Brute force approach is to calculate the area for every pair of lines and find the maximum area out of those o(n^2).
+# We can solve this problem in O(n) time using two pointers approach.
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+        l, r = 0, len(height)-1
+        while l < r:
+            area = (r-l) * min(height[l], height[r])
+            res = max(res, area)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
