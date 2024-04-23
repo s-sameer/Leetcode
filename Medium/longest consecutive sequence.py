1cf30ce7b22ad7b47be448e3b0b8fd9382e17e83
Sadman Sameer <itsnotsameer@gmail.com>
2024-04-23 00:00:33 -0400

    added additional info

--- Medium/longest consecutive sequence.py
+++ Medium/longest consecutive sequence.py
@@ -7,7 +7,7 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         myset = set(nums)
         longest = 0
-        
+        # check if its the start of the sequence (is there a left neighbour?)
         for num in nums:
             if (num-1) not in myset:
                 length = 0
